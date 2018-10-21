3bf1e1821156b9dc76f76688f961421d015d6d92
Reggosch <Reggosch@gmx.de>
2018-10-21 23:29:46 +0200

    mal-durch 2 btn

--- hilow.js
+++ hilow.js
@@ -1,3 +1,5 @@
+// Hi-Low Game v0.1
+//
 var summe = 0;
 var rnd = 0;
 var kto = 1000;
@@ -6,10 +8,24 @@ var einsatz = 10;
 var btn1 = document.getElementById('btn1');
 var btn2 = document.getElementById('btn2');
 var btnSumme = document.getElementById('summe');
+var btnMal2 = document.getElementById('btnEinsatzMal2');
+var btnDurch2 = document.getElementById('btnEinsatzDurch2');
 var txtKto = document.getElementById('txtKto');
+var txtEinsatz = document.getElementById('txtEinsatz');
 
 btn1.addEventListener('click', btn1Click);
 btn2.addEventListener('click', btn2Click);
+btnMal2.addEventListener('click',btnMal2Click);
+btnDurch2.addEventListener('click',btnDurch2Click);
+
+function btnMal2Click() {
+  einsatz *= 2;
+  txtEinsatz.textContent = 'Einsatz: '+einsatz;
+}
+function btnDurch2Click() {
+  einsatz /= 2;
+  txtEinsatz.textContent = 'Einsatz: '+einsatz;
+}
 
 function btn1Click() {
     rnd = myRnd();
